1a68f39490f408286ff9c29c1d716ad0a95440c6
desenmecf <desenmecf@yahoo.com.br>
2013-10-16 20:47:01 -0300

    Update Contato.java

--- Contato.java
+++ Contato.java
@@ -1,9 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package src;
 
+import java.util.Vector;
 
 public class Contato {
+    private String nome,telefone,email;
+    private Vector contato;
+
+    public Contato(){
+        contato = new Vector();        
+    }
     
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }    
 }
